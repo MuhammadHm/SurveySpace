394f6ad153c8a296bfbcc59000646db00707e3c9
mohammed Ayash <mhma.sy994@gmail.com>
2019-05-30 11:30:32 +0300

    mhd

--- models/results.js
+++ models/results.js
@@ -9,14 +9,39 @@ module.exports=class Result{
     async addanswer (survey_id , user_id , answers){
         //Attributes    
         this.answers = answers;
+        console.log("12",answers.answer);
         // readFileResult 
         let path1=path.join(__dirname,'..','dataBase','results',`${survey_id}.json`);
         let answerVisitor=[];
-        fs.readFile(path1,(err,content)=>{
+        fs.readFile(path1,async(err,content)=>{
             answerVisitor=JSON.parse(content);
             answerVisitor.answer.push(this);
+                for (let i=0;i<answers.lenght();i++)
+                    {  if (answers[i].answer != undefined)
+                        {   answerVisitor.result[i].count++;
+                            if (answers[i].questionType === "textbox" || answers[i].questionType == "essay")
+                            answerVisitor.result[i].report.push(answers.answer);
+                            else if (answers[i].questionType ==="checkbox" || answers[i].questionType ==="mulchoice")  
+                          {   oneresult.check ={};
+                              answers[i].answer.forEach(element =>{
+                                if(element.checked === true)
+                                answerVisitor.result[i].check[element.body]++; 
+                          });
+                          }   
+                      else if (answers[i].questionType ==="scale")
+                            answerVisitor.result[i].scale+=answers[i].answer;
+                      else if (answers[i].questionType === "date")
+                            answerVisitor.result[i].date.push(answer);       
+                        }
+                    }   
+                    console.log(answerVisitor);
             answerVisitor=JSON.stringify(answerVisitor);
-            fs.writeFileSync(path1,answerVisitor);
+            await fs.writeFileSync(path1,answerVisitor);
             });
     }
+
+    
+
+
+
 }
--- models/survey.js
+++ models/survey.js
@@ -22,11 +22,6 @@ module.exports=class Survey{
         let jsonSurvey=JSON.stringify(this);
         console.log("writing survey whith id " ,this.id);
         
-        //create file for rueslt survey
-        let jsonAnswer=JSON.stringify(answervisitor);
-        console.log(jsonAnswer);
-        await fs.writeFileSync(path.join(__dirname,'..','dataBase','results',`${this.id}.json`),jsonAnswer);
-
        
 
         await fs.writeFileSync(path.join(__dirname,'..','dataBase','survey',`${this.id}.json`),jsonSurvey);
@@ -100,8 +95,8 @@ module.exports=class Survey{
             result :result,
             answer:[]
         };
-        let answerVisitors=JSON.stringify(answerVisitor);
-        console.log( "2",answerVisitors);
+        answerVisitor=JSON.stringify(answerVisitor);
+        console.log( "2",answerVisitor);
         fs.writeFileSync(path1,answerVisitor);
            
     }
