492395ac8c2afdb4d605427484b4e36f2e66d031
MuhammadHm <mohamad.1999.syr@gmail.com>
2019-06-14 14:03:11 +0300

    hm

--- routs/back.js
+++ routs/back.js
@@ -28,7 +28,7 @@ rout.use("/:id",async (req,res,next)=>{
     req.session.user=user;
 
     
-    res.status(200).render('home',{ isAuth : true, path : "/home" ,lang : lang});
+    res.status(200).render('home',{ isAuth : true, path : "/home" ,lang : lang, language : "en"});
 
 });
 
